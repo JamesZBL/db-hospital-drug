3bd8c91750543e43b6fd27869b5909cdfbbb24f3
James <18333298410@163.com>
2018-05-07 17:24:30 +0800

    生成药品系统相关实体映射

---
+++ src/main/java/me/zbl/app/domain/Supplier.java
@@ -0,0 +1,23 @@
+package me.zbl.app.domain;
+
+public class Supplier {
+    private String id;
+
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+}
