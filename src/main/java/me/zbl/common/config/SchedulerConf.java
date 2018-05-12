59b498551c9e674835d16c1a77f3de541ca11308
James <18333298410@163.com>
2018-05-12 20:27:37 +0800

    增加 Spring Scheduling 配置，实现了每天凌晨检查过期药品并发送提醒消息

---
+++ src/main/java/me/zbl/common/config/SchedulerConf.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2018 JamesZBL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package me.zbl.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author JamesZBL
+ * @email 1146556298@qq.com
+ * @date 2018-05-12
+ */
+@Configuration
+@EnableScheduling
+public class SchedulerConf {
+
+}
